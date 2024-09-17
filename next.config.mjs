4b89d44a8025780892daaadbe048be6692af0c09
victortimsit <victortimsit.contact@gmail.com>
2024-09-17 18:57:55 +0200

    Update Cross-Origin-Embedder-Policy and Cross-Origin-Opener-Policy headers in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,12 +6,12 @@ const nextConfig = {
         source: "/:path*", // Apply these headers to all routes
         headers: [
           {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin",
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp", // or "credentialless"
           },
           {
-            key: "Cross-Origin-Embedder-Policy",
-            value: "require-corp",
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
           },
         ],
       },
